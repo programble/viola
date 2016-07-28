a118997558f84fed9463bcfea273a1fa156fc133
Curtis McEnroe <programble@gmail.com>
2016-07-28 19:02:47 -0400

    Enable warnings and unstable features

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+//! Vi-like editor.
+
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    variant_size_differences,
+)]
+
+#![feature(
+    associated_consts,
+    box_patterns,
+    box_syntax,
+    const_fn,
+    inclusive_range_syntax,
+    question_mark,
+    specialization,
+    type_ascription,
+)]
