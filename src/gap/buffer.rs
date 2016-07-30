7985605daabf788f2a4034600ca040bc65f41a24
Curtis McEnroe <programble@gmail.com>
2016-07-29 22:15:43 -0400

    Refactor entire GapBuffer interface

--- src/gap/buffer.rs
+++ src/gap/buffer.rs
@@ -25,6 +25,21 @@ impl GapBuffer {
         buffer
     }
 
+    /// Returns the length of the buffer (excluding gap).
+    pub fn len(&self) -> usize {
+        self.buf.len() - self.gap.len()
+    }
+
+    /// Returns the length of the gap.
+    pub fn gap_len(&self) -> usize {
+        self.gap.len()
+    }
+
+    /// Returns the two byte slices before and after the gap.
+    pub fn as_slices(&self) -> (&[u8], &[u8]) {
+        (&self.buf[self.gap.before()], &self.buf[self.gap.after()])
+    }
+
     fn resize_buf(&mut self, additional: usize) {
         let new_len = self.buf.len() + additional;
         self.buf.reserve_exact(additional);
@@ -39,93 +54,77 @@ impl GapBuffer {
         unsafe { self.buf.as_ptr().offset(self.gap.end as isize) }
     }
 
-    /// Returns the length of the buffer (excluding gap).
-    pub fn len(&self) -> usize {
-        self.buf.len() - self.gap.len()
+    fn move_gap_up(&mut self, index: usize) {
+        let move_len = index - self.gap.start;
+        unsafe {
+            ptr::copy(self.gap_end(), self.gap_start() as *mut u8, move_len);
+        }
+        self.gap.start += move_len;
+        self.gap.end += move_len;
     }
 
-    /// Returns the length of the gap.
-    pub fn gap_len(&self) -> usize {
-        self.gap.len()
+    fn move_gap_down(&mut self, index: usize) {
+        let move_len = self.gap.start - index;
+        unsafe {
+            ptr::copy(
+                self.buf.as_ptr().offset(index as isize),
+                self.gap_end().offset(-(move_len as isize)) as *mut u8,
+                move_len,
+            );
+        }
+        self.gap.start -= move_len;
+        self.gap.end -= move_len;
     }
 
-    /// Returns the index of the start of the gap.
-    pub fn gap_index(&self) -> usize {
-        self.gap.start
+    fn resize_to_fit(&mut self, fit: usize) {
+        // Allocate enough for `fit` and new gap.
+        let old_len = self.buf.len();
+        let gap_len = (self.len() + fit) / 2;
+        let additional = fit - self.gap.len() + gap_len;
+        self.resize_buf(additional);
+
+        // Move data after gap to the end of the buffer. This is a bit inefficient since the Vec
+        // has already copied this data once.
+        unsafe {
+            ptr::copy(
+                self.gap_end(),
+                self.gap_end().offset(additional as isize) as *mut u8,
+                old_len - self.gap.end,
+            );
+        }
+
+        self.gap.end += additional;
     }
 
-    /// Returns the two byte slices before and after the gap.
-    pub fn as_slices(&self) -> (&[u8], &[u8]) {
-        (&self.buf[self.gap.before()], &self.buf[self.gap.after()])
+    fn copy_into_gap(&mut self, src: &[u8]) {
+        let dest = &mut self.buf[self.gap.resize_end(src.len())];
+        dest.copy_from_slice(src);
+        self.gap.start += src.len();
     }
 
-    /// Moves the gap so that it starts at `index`.
+    /// Replaces the bytes at `dest` with the bytes in `src`.
+    ///
+    /// Returns the range of the written bytes.
     ///
     /// # Panics
     ///
-    /// Panics if `index` is out of bounds.
-    pub fn move_gap(&mut self, index: usize) {
-        if index > self.gap.start {
-            assert!(index <= self.len(), "gap index out of bounds");
-
-            let move_len = index - self.gap.start;
-            unsafe {
-                ptr::copy(self.gap_end(), self.gap_start() as *mut u8, move_len);
-            }
-
-            self.gap.start += move_len;
-            self.gap.end += move_len;
-
-        } else if index < self.gap.start {
-            let move_len = self.gap.start - index;
-            unsafe {
-                ptr::copy(
-                    self.buf.as_ptr().offset(index as isize),
-                    self.gap_end().offset(-(move_len as isize)) as *mut u8,
-                    move_len,
-                );
-            }
-
-            self.gap.start -= move_len;
-            self.gap.end -= move_len;
-        }
-    }
-
-    /// Inserts bytes at the start of the gap. If the bytes cannot fit in the current gap, a new
-    /// one is allocated of half the total size of the buffer.
-    ///
-    /// Returns the new index of the start of the gap.
-    pub fn insert(&mut self, src: &[u8]) -> usize {
-        if src.len() < self.gap.len() {
-            let dest = &mut self.buf[self.gap.resize_end(src.len())];
-            dest.copy_from_slice(src);
-            self.gap.start += src.len();
-
-        } else {
-            // Allocate additional space for `src` and a new gap.
-            let old_len = self.buf.len();
-            let gap_len = (self.len() + src.len()) / 2;
-            let additional = src.len() - self.gap.len() + gap_len;
-            self.resize_buf(additional);
-
-            // Move data after gap to end of buffer.
-            unsafe {
-                ptr::copy(
-                    self.gap_end(),
-                    self.gap_end().offset(additional as isize) as *mut u8,
-                    old_len - self.gap.end,
-                );
-            }
-
-            // Copy in bytes.
-            let dest = &mut self.buf[self.gap.resize_end(src.len())];
-            dest.copy_from_slice(src);
-
-            // Set newly allocated gap.
-            self.gap.start += src.len();
-            self.gap.end += additional;
+    /// Panics if `dest` is out of bounds.
+    pub fn replace(&mut self, dest: ByteRange, src: &[u8]) -> ByteRange {
+        if dest.start > self.gap.start {
+            assert!(dest.start <= self.len(), "dest start out of bounds");
+            self.move_gap_up(dest.start);
+        } else if dest.start < self.gap.start {
+            self.move_gap_down(dest.start);
         }
 
-        self.gap.start
+        assert!(self.gap.end + dest.len() <= self.buf.len(), "dest end out of bounds");
+        self.gap.end += dest.len();
+
+        if src.len() >= self.gap.len() {
+            self.resize_to_fit(src.len());
+        }
+        self.copy_into_gap(src);
+
+        dest.resize_end(src.len())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,16 @@ use viola::gap::GapBuffer;
 fn main() {
     let mut buf = GapBuffer::new();
     println!("{:?}", buf);
-    buf.insert(&[4, 5, 6]);
+    buf.replace((0..0).into(), &[2]);
     println!("{:?}", buf);
-    buf.move_gap(0);
+    buf.replace((0..0).into(), &[1]);
     println!("{:?}", buf);
-    buf.insert(&[1, 2]);
+    buf.replace((0..2).into(), &[]);
     println!("{:?}", buf);
-    buf.move_gap(5);
+    buf.replace((0..0).into(), &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     println!("{:?}", buf);
-    buf.insert(&[7, 8, 9]);
+    buf.replace((4..8).into(), &[11, 12]);
     println!("{:?}", buf);
-    buf.move_gap(2);
-    println!("{:?}", buf);
-    buf.insert(&[3]);
+    buf.replace((6..6).into(), &[0, 0, 0, 0, 0, 0]);
     println!("{:?}", buf);
 }
