920fa0c3b958c831cd5f0fc5effea11a7ef96aae
Curtis McEnroe <programble@gmail.com>
2016-07-30 19:48:10 -0400

    Refactor quickcheck tests and improve inputs

---
+++ tests/common/mod.rs
@@ -0,0 +1,56 @@
+use std::ops::Range;
+
+use quickcheck::{Arbitrary, Gen};
+use viola::gap::{GapBuffer, GapString};
+
+// A `Range<usize>` where start <= end. Helps to generate more valid splice operations.
+#[derive(Debug, Clone)]
+pub struct SliceRange(pub Range<usize>);
+
+impl Arbitrary for SliceRange {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        let start = usize::arbitrary(g);
+        let size = g.size();
+        let end = g.gen_range(start, start + size);
+        SliceRange(start..end)
+    }
+
+    fn shrink(&self) -> Box<Iterator<Item = Self>> {
+        box self.0.shrink().map(SliceRange)
+    }
+}
+
+// Trait for dispatching to gap buffer splice and naive Vec/String implementations.
+pub trait Splice<S: ?Sized> {
+    fn splice(&mut self, dest: SliceRange, src: &S);
+}
+
+impl Splice<[u8]> for GapBuffer {
+    fn splice(&mut self, dest: SliceRange, src: &[u8]) {
+        self.splice(dest.0, src);
+    }
+}
+
+impl Splice<str> for GapString {
+    fn splice(&mut self, dest: SliceRange, src: &str) {
+        self.splice(dest.0, src);
+    }
+}
+
+impl Splice<[u8]> for Vec<u8> {
+    fn splice(&mut self, dest: SliceRange, src: &[u8]) {
+        self.drain(dest.0.clone());
+
+        // Inefficient, but works.
+        for &byte in src.iter().rev() {
+            self.insert(dest.0.start, byte);
+        }
+    }
+}
+
+impl Splice<str> for String {
+    fn splice(&mut self, dest: SliceRange, src: &str) {
+        self.drain(dest.0.clone());
+        self.insert_str(dest.0.start, src);
+    }
+}
--- tests/gap_string.rs
+++ tests/gap_string.rs
@@ -1,75 +1,60 @@
-#![feature(insert_str, plugin, type_ascription)]
+#![feature(
+    box_syntax,
+    insert_str,
+    plugin,
+    type_ascription,
+)]
+
 #![plugin(quickcheck_macros)]
 
 extern crate quickcheck;
 extern crate viola;
 
-use std::ops::Range;
+mod common;
 
 use quickcheck::TestResult;
 use viola::gap::GapString;
 
-trait Splice {
-    fn splice(&mut self, dest: Range<usize>, src: &str);
-}
-
-impl Splice for GapString {
-    fn splice(&mut self, dest: Range<usize>, src: &str) {
-        self.splice(dest, src);
-    }
-}
-
-impl Splice for String {
-    fn splice(&mut self, dest: Range<usize>, src: &str) {
-        self.drain(dest.clone());
-        self.insert_str(dest.start, src);
-    }
-}
-
-fn valid_dest(dest: &Range<usize>, string: &str) -> bool {
-    dest.start <= dest.end
-        && string.is_char_boundary(dest.start)
-        && string.is_char_boundary(dest.end)
-}
+use common::{SliceRange, Splice};
 
 #[quickcheck]
-fn empty_one_splice(src: String) -> bool {
+fn empty_splice(src: String) -> bool {
     let mut string = String::new();
     let mut buffer = GapString::new();
 
-    string.splice(0..0, &src);
-    buffer.splice(0..0, &src);
+    Splice::splice(&mut string, SliceRange(0..0), &src);
+    Splice::splice(&mut buffer, SliceRange(0..0), &src);
 
     string == buffer.into(): String
 }
 
 #[quickcheck]
-fn one_splice(init: String, dest: Range<usize>, src: String) -> TestResult {
-    if !valid_dest(&dest, &init) {
+fn splice(init: String, dest: SliceRange, src: String) -> TestResult {
+    if !init.is_char_boundary(dest.0.start) || !init.is_char_boundary(dest.0.end) {
         return TestResult::discard();
     }
 
     let mut string = init.clone();
     let mut buffer = GapString::from(init);
 
-    string.splice(dest.clone(), &src);
-    buffer.splice(dest, &src);
+    Splice::splice(&mut string, dest.clone(), &src);
+    Splice::splice(&mut buffer, dest, &src);
 
     TestResult::from_bool(string == buffer.into(): String)
 }
 
 #[quickcheck]
-fn multi_splice(init: String, splices: Vec<(Range<usize>, String)>) -> TestResult {
+fn splice_seq(init: String, splices: Vec<(SliceRange, String)>) -> TestResult {
     let mut string = init.clone();
     let mut buffer = GapString::from(init);
 
     for (dest, src) in splices {
-        if !valid_dest(&dest, &string) {
+        if !string.is_char_boundary(dest.0.start) || !string.is_char_boundary(dest.0.end) {
             return TestResult::discard();
         }
 
-        string.splice(dest.clone(), &src);
-        buffer.splice(dest, &src);
+        Splice::splice(&mut string, dest.clone(), &src);
+        Splice::splice(&mut buffer, dest, &src);
     }
 
     TestResult::from_bool(string == buffer.into(): String)
