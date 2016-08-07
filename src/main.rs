3ddfd351b22c04aaf81fec779de9165448e8ff5c
Curtis McEnroe <programble@gmail.com>
2016-08-07 17:25:11 -0400

    Uncomment main

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 extern crate viola;
 
-//use viola::gap::string::GapString;
+use viola::gap::String;
 
 fn main() {
-    //let mut buf = GapString::new();
-    //println!("{:?}", buf);
-    //buf.splice(.., "Good, ");
-    //println!("{:?}", buf);
-    //buf.splice(6.., "world!");
-    //println!("{:?}", buf);
-    //buf.splice(4..4, "bye");
-    //println!("{:?}", buf);
-    //buf.splice(..7, "hello");
-    //println!("{:?}", buf);
+    let mut buf = String::new();
+    println!("{:?}", buf);
+    buf.splice(.., "Good, ");
+    println!("{:?}", buf);
+    buf.splice(6.., "world!");
+    println!("{:?}", buf);
+    buf.splice(4..4, "bye");
+    println!("{:?}", buf);
+    buf.splice(..7, "hello");
+    println!("{:?}", buf);
 
-    //println!("{:?} {:?} {:?}", buf.slice(..), buf.slice(..5), buf.slice(5..));
-    //println!("{:?} {:?} {:?}", buf.slice(1..7), buf.slice(1..3), buf.slice(6..10));
+    println!("{:?} {:?} {:?}", buf.slice(..), buf.slice(..5), buf.slice(5..));
+    println!("{:?} {:?} {:?}", buf.slice(1..7), buf.slice(1..3), buf.slice(6..10));
 
-    //for x in buf.slice(..).char_indices() {
-    //    print!("{:?} ", x);
-    //}
-    //println!("");
+    for x in buf.slice(..).char_indices() {
+        print!("{:?} ", x);
+    }
+    println!("");
 
-    //for x in buf.slice(..).char_indices().rev() {
-    //    print!("{:?} ", x);
-    //}
-    //println!("");
+    for x in buf.slice(..).char_indices().rev() {
+        print!("{:?} ", x);
+    }
+    println!("");
 
-    //buf.splice(..1, "H");
-    //println!("{:?}", buf);
+    buf.splice(..1, "H");
+    println!("{:?}", buf);
 
-    //println!("{}", buf);
+    println!("{}", buf);
 }
