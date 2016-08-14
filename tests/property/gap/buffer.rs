44eab448940d0f36219e3bcd0366c62a8f47b1c2
Curtis McEnroe <programble@gmail.com>
2016-08-14 00:44:20 -0400

    Rewrite Buffer property tests

--- tests/property/gap/buffer.rs
+++ tests/property/gap/buffer.rs
@@ -1,104 +1,151 @@
 use quickcheck::TestResult;
 use viola::gap::Buffer;
-use viola::range::IntoRange;
 
 use super::{SliceRange, Splice};
 
-#[quickcheck]
-fn from_vec_into_vec(vec: Vec<u8>) -> bool {
-    let buf = Buffer::from(vec.clone());
-    vec == buf.into(): Vec<u8>
+macro_rules! discard {
+    ($range:expr, $slice:expr) => {
+        if !$range.valid_slice($slice) {
+            return TestResult::discard();
+        }
+    }
 }
 
 #[quickcheck]
-fn from_slice_into_vec(vec: Vec<u8>) -> bool {
-    let buf = Buffer::from(vec.as_slice());
-    vec == buf.into(): Vec<u8>
+fn from_vec(init: Vec<u8>) -> bool {
+    let buf = Buffer::from(init.clone());
+    buf == init
 }
 
 #[quickcheck]
-fn slice(vec: Vec<u8>, range: SliceRange) -> TestResult {
-    if !range.valid_slice(&vec) { return TestResult::discard(); }
-
-    let buf = Buffer::from(vec.as_slice());
-
-    let vec_slice = &vec[range.into_range(0)];
-    let buf_slice = buf.slice(range);
-
-    TestResult::from_bool(buf_slice == vec_slice)
+fn from_slice(init: Vec<u8>) -> bool {
+    let buf = Buffer::from(&init[..]);
+    buf == init
 }
 
 #[quickcheck]
-fn slice_slice(vec: Vec<u8>, first: SliceRange, second: SliceRange) -> TestResult {
-    if !first.valid_slice(&vec) { return TestResult::discard(); }
-
-    let buf = Buffer::from(vec.as_slice());
-
-    let vec_first = &vec[first.into_range(0)];
-    let buf_first = buf.slice(first);
-
-    if !second.valid_slice(vec_first) { return TestResult::discard(); }
-
-    let vec_second = &vec_first[second.into_range(0)];
-    let buf_second = buf_first.slice(second);
-
-    TestResult::from_bool(buf_second == vec_second)
+fn into_vec(init: Vec<u8>) -> bool {
+    let buf = Buffer::from(&init[..]);
+    buf.into(): Vec<u8> == init
 }
 
 #[quickcheck]
-fn slice_iter(vec: Vec<u8>, range: SliceRange) -> TestResult {
-    if !range.valid_slice(&vec) { return TestResult::discard(); }
+fn len(init: Vec<u8>) -> bool {
+    let buf = Buffer::from(&init[..]);
+    buf.len() == init.len()
+}
 
-    let buf = Buffer::from(vec.as_slice());
+#[quickcheck]
+fn slice(init: Vec<u8>, range: SliceRange) -> TestResult {
+    discard!(range, &init);
+    let buf = Buffer::from(&init[..]);
+    TestResult::from_bool(buf.slice(range) == init[range])
+}
 
-    let vec_iter = vec[range.into_range(0)].iter();
-    let buf_iter = buf.slice(range).into_iter();
+#[quickcheck]
+fn slice_slice(init: Vec<u8>, one: SliceRange, two: SliceRange) -> TestResult {
+    discard!(one, &init);
+    let buf = Buffer::from(&init[..]);
 
-    TestResult::from_bool(vec_iter.eq(buf_iter))
+    let vec_one = &init[one];
+    let buf_one = buf.slice(one);
+    discard!(two, vec_one);
+
+    TestResult::from_bool(buf_one.slice(two) == vec_one[two])
+}
+
+#[quickcheck]
+fn slice_iter(init: Vec<u8>, range: SliceRange) -> TestResult {
+    discard!(range, &init);
+    let buf = Buffer::from(&init[..]);
+    TestResult::from_bool(buf.slice(range).into_iter().eq(init[range].iter()))
+}
+
+#[quickcheck]
+fn slice_into_vec(init: Vec<u8>, range: SliceRange) -> TestResult {
+    discard!(range, &init);
+    let buf = Buffer::from(&init[..]);
+    TestResult::from_bool(buf.slice(range).into(): Vec<u8> == &init[range])
 }
 
 #[quickcheck]
 fn splice(init: Vec<u8>, dest: SliceRange, src: Vec<u8>) -> TestResult {
-    if !dest.valid_slice(&init) { return TestResult::discard(); }
-
+    discard!(dest, &init);
     let mut vec = init.clone();
     let mut buf = Buffer::from(init);
 
     Splice::splice(&mut vec, dest, &src);
     Splice::splice(&mut buf, dest, &src);
 
-    TestResult::from_bool(vec == buf.into(): Vec<u8>)
+    TestResult::from_bool(buf == vec)
 }
 
 #[quickcheck]
-fn splice_seq(init: Vec<u8>, splices: Vec<(SliceRange, Vec<u8>)>) -> TestResult {
+fn splice_splice(
+    init: Vec<u8>,
+    one: (SliceRange, Vec<u8>),
+    two: (SliceRange, Vec<u8>),
+) -> TestResult {
+    discard!(one.0, &init);
     let mut vec = init.clone();
     let mut buf = Buffer::from(init);
 
-    for (dest, src) in splices {
-        if !dest.valid_slice(&vec) { return TestResult::discard(); }
+    Splice::splice(&mut vec, one.0, &one.1);
+    Splice::splice(&mut buf, one.0, &one.1);
 
-        Splice::splice(&mut vec, dest, &src);
-        Splice::splice(&mut buf, dest, &src);
-    }
+    discard!(two.0, &vec);
 
-    TestResult::from_bool(vec == buf.into(): Vec<u8>)
+    Splice::splice(&mut vec, two.0, &two.1);
+    Splice::splice(&mut buf, two.0, &two.1);
+
+    TestResult::from_bool(buf == vec)
 }
 
 #[quickcheck]
-fn splice_slice(init: Vec<u8>, dest: SliceRange, src: Vec<u8>, slice: SliceRange) -> TestResult {
-    if !dest.valid_slice(&init) { return TestResult::discard(); }
-
+fn splice_slice(init: Vec<u8>, dest: SliceRange, src: Vec<u8>, range: SliceRange) -> TestResult {
+    discard!(dest, &init);
     let mut vec = init.clone();
     let mut buf = Buffer::from(init);
 
     Splice::splice(&mut vec, dest, &src);
     Splice::splice(&mut buf, dest, &src);
 
-    if !slice.valid_slice(&vec) { return TestResult::discard(); }
+    discard!(range, &vec);
+    TestResult::from_bool(buf.slice(range) == vec[range])
+}
 
-    let vec_slice = &vec[slice.into_range(0)];
-    let buf_slice = buf.slice(slice);
+#[quickcheck]
+fn splice_slice_iter(
+    init: Vec<u8>,
+    dest: SliceRange,
+    src: Vec<u8>,
+    range: SliceRange,
+) -> TestResult {
+    discard!(dest, &init);
+    let mut vec = init.clone();
+    let mut buf = Buffer::from(init);
 
-    TestResult::from_bool(buf_slice == vec_slice)
+    Splice::splice(&mut vec, dest, &src);
+    Splice::splice(&mut buf, dest, &src);
+
+    discard!(range, &vec);
+    TestResult::from_bool(buf.slice(range).into_iter().eq(vec[range].iter()))
+}
+
+#[quickcheck]
+fn splice_slice_into_vec(
+    init: Vec<u8>,
+    dest: SliceRange,
+    src: Vec<u8>,
+    range: SliceRange,
+) -> TestResult {
+    discard!(dest, &init);
+    let mut vec = init.clone();
+    let mut buf = Buffer::from(init);
+
+    Splice::splice(&mut vec, dest, &src);
+    Splice::splice(&mut buf, dest, &src);
+
+    discard!(range, &vec);
+    TestResult::from_bool(buf.slice(range).into(): Vec<u8> == &vec[range])
 }
--- tests/property/gap/mod.rs
+++ tests/property/gap/mod.rs
@@ -1,7 +1,6 @@
 mod buffer;
-mod string;
 
-use std::ops::Range;
+use std::ops::{Index, Range};
 use std::string::String as StdString;
 
 use quickcheck::{Arbitrary, Gen};
@@ -32,6 +31,38 @@ impl IntoRange for SliceRange {
     }
 }
 
+impl Index<SliceRange> for Vec<u8> {
+    type Output = [u8];
+
+    fn index(&self, index: SliceRange) -> &[u8] {
+        &self[index.start..index.end]
+    }
+}
+
+impl Index<SliceRange> for [u8] {
+    type Output = [u8];
+
+    fn index(&self, index: SliceRange) -> &[u8] {
+        &self[index.start..index.end]
+    }
+}
+
+impl Index<SliceRange> for StdString {
+    type Output = str;
+
+    fn index(&self, index: SliceRange) -> &str {
+        &self[index.start..index.end]
+    }
+}
+
+impl Index<SliceRange> for str {
+    type Output = str;
+
+    fn index(&self, index: SliceRange) -> &str {
+        &self[index.start..index.end]
+    }
+}
+
 impl Arbitrary for SliceRange {
     fn arbitrary<G: Gen>(g: &mut G) -> Self {
         let start = usize::arbitrary(g);
@@ -53,7 +84,6 @@ impl Arbitrary for SliceRange {
     }
 }
 
-// Trait for dispatching to gap buffer splice and naive `Vec` / `StdString` implementations.
 trait Splice<S: ?Sized> {
     fn splice(&mut self, dest: SliceRange, src: &S);
 }
@@ -64,26 +94,26 @@ impl Splice<[u8]> for Buffer {
     }
 }
 
-impl Splice<[u8]> for Vec<u8> {
-    fn splice(&mut self, dest: SliceRange, src: &[u8]) {
-        self.drain(dest.into_range(0));
-
-        // Inefficient, but works.
-        for &byte in src.iter().rev() {
-            self.insert(dest.start, byte);
-        }
-    }
-}
-
 impl Splice<str> for String {
     fn splice(&mut self, dest: SliceRange, src: &str) {
         self.splice(dest, src);
     }
 }
 
+impl Splice<[u8]> for Vec<u8> {
+    fn splice(&mut self, dest: SliceRange, src: &[u8]) {
+        self.drain(dest.start..dest.end);
+
+        // Inefficient, but works. Where is insert_slice?
+        for &byte in src.iter().rev() {
+            self.insert(dest.start, byte);
+        }
+    }
+}
+
 impl Splice<str> for StdString {
     fn splice(&mut self, dest: SliceRange, src: &str) {
-        self.drain(dest.into_range(0));
+        self.drain(dest.start..dest.end);
         self.insert_str(dest.start, src);
     }
 }
